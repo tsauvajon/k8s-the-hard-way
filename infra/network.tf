88a2ab28a05545ae28cd16e2b357f205894da8d6
Thomas Sauvajon <thomas@flurosat.com>
2019-11-22 22:40:47 +1100

    Fix a typo in CIDR

--- infra/network.tf
+++ infra/network.tf
@@ -24,7 +24,7 @@ resource "google_compute_firewall" "internal" {
     protocol = "udp"
   }
 
-  source_ranges = ["10.240.0.0/24, 10.200.0.0/16"]
+  source_ranges = ["10.240.0.0/24", "10.200.0.0/16"]
 }
 
 resource "google_compute_firewall" "external" {
