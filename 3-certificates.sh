0c568d5a8df46ab9b67a4e8843b82a8ae2593017
Thomas Sauvajon <thomas@flurosat.com>
2019-11-23 16:59:28 +1100

    Fix names and outputs using a remote state

--- 3-certificates.sh
+++ 3-certificates.sh
@@ -81,7 +81,7 @@ cfssl gencert \
 
 
 ################### Kubelets ###################
-for instance in worker_0 worker_1 worker_2; do
+for instance in worker-0 worker-1 worker-2; do
 cat > ${instance}-csr.json <<EOF
 {
   "CN": "system:node:${instance}",
@@ -274,12 +274,12 @@ cfssl gencert \
 
 
 ################### Install certificates ###################
-for instance in worker_0 worker_1 worker_2; do
+for instance in worker-0 worker-1 worker-2; do
   name=$(cd ../infra; terraform output ${instance}_name)
   gcloud compute scp ca.pem ${instance}-key.pem ${instance}.pem ${name}:~/
 done
 
-for instance in controller_0 controller_1 controller_2; do
+for instance in controller-0 controller-1 controller-2; do
   name=$(cd ../infra; terraform output ${instance}_name)
   gcloud compute scp ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
     service-account-key.pem service-account.pem ${name}:~/
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,51 +1,51 @@
-output "controller_0_name" {
+output "controller-0_name" {
   value       = google_compute_instance.controller_0.name
   description = "Name of the controller_0 instance"
 }
-output "controller_1_name" {
+output "controller-1_name" {
   value       = google_compute_instance.controller_1.name
   description = "Name of the controller_1 instance"
 }
-output "controller_2_name" {
+output "controller-2_name" {
   value       = google_compute_instance.controller_2.name
   description = "Name of the controller_2 instance"
 }
 
-output "worker_0_name" {
+output "worker-0_name" {
   value       = google_compute_instance.worker_0.name
   description = "Name of the worker_0 instance"
 }
-output "worker_1_name" {
+output "worker-1_name" {
   value       = google_compute_instance.worker_1.name
   description = "Name of the worker_1 instance"
 }
-output "worker_2_name" {
+output "worker-2_name" {
   value       = google_compute_instance.worker_2.name
   description = "Name of the worker_2 instance"
 }
 
-output "worker_0_internal_ip" {
+output "worker-0_internal_ip" {
   value       = google_compute_instance.worker_0.network_interface.0.network_ip
   description = "Internal IP of the worker_0 instance"
 }
-output "worker_1_internal_ip" {
+output "worker-1_internal_ip" {
   value       = google_compute_instance.worker_1.network_interface.0.network_ip
   description = "Internal IP of the worker_1 instance"
 }
-output "worker_2_internal_ip" {
+output "worker-2_internal_ip" {
   value       = google_compute_instance.worker_2.network_interface.0.network_ip
   description = "Internal IP of the worker_2 instance"
 }
 
-output "worker_0_external_ip" {
+output "worker-0_external_ip" {
   value       = google_compute_instance.worker_0.network_interface.0.access_config.0.nat_ip
   description = "External IP of the worker_0 instance"
 }
-output "worker_1_external_ip" {
+output "worker-1_external_ip" {
   value       = google_compute_instance.worker_1.network_interface.0.access_config.0.nat_ip
   description = "External IP of the worker_1 instance"
 }
-output "worker_2_external_ip" {
+output "worker-2_external_ip" {
   value       = google_compute_instance.worker_2.network_interface.0.access_config.0.nat_ip
   description = "External IP of the worker_2 instance"
 }
