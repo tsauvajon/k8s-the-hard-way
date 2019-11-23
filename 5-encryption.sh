88ab6221fddcfd53a20a0d612c4907f94eb6f31e
Thomas Sauvajon <thomas@flurosat.com>
2019-11-23 17:03:29 +1100

    Encrypt data at rest

---
+++ 5-encryption.sh
@@ -0,0 +1,20 @@
+ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+
+mkdir -p encryption
+cat > encryption/encryption-config.yaml <<EOF
+kind: EncryptionConfig
+apiVersion: v1
+resources:
+  - resources:
+      - secrets
+    providers:
+      - aescbc:
+          keys:
+            - name: key1
+              secret: ${ENCRYPTION_KEY}
+      - identity: {}
+EOF
+
+for instance in controller-0 controller-1 controller-2; do
+  gcloud compute scp encryption/encryption-config.yaml ${instance}:~/
+done
